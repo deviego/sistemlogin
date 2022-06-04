95018c4f86e8184f8d3c9b7991f35ce197718322
Deviego <diegodomingues266@gmail.com>
2022-06-03 21:42:20 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import { Link, Route, Routes } from "react-router-dom";
+import "./App.css";
+import {Home} from './pages/Home';
+import {Private} from './pages/Private';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <h1>Página de login</h1>
+        <nav>
+          <Link to={"/"} > Home</Link>
+          <Link to={"/private"}>Private</Link>
+        </nav>
+      </header>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/private" element={<Private/>} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
